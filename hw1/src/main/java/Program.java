192080aeddbb250faf7a7bcdab3aeefa484df263
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2017-01-26 18:05:49 +0100

    put regions around parsers in program.java

--- hw1/src/main/java/Program.java
+++ hw1/src/main/java/Program.java
@@ -61,12 +61,16 @@ public class Program
 
         try
         {
-            // DOM parser - company info
+            //region DOM parser - company info
+
             DOMParser domParser = new DOMParser();
             ArrayList<Company> companies = domParser.parseCompanyInfoXml(companyInfoFileInputStream);
             System.out.println("Parsed companies: " + companies.toString());
 
-            // SAX Parser - employment record
+            //endregion
+
+            //region SAX Parser - employment record
+
             SAXHandler saxHandler = new SAXHandler();
             SAXParserFactory saxFactory = SAXParserFactory.newInstance();
             SAXParser saxParser = saxFactory.newSAXParser();
@@ -78,21 +82,32 @@ public class Program
                     .collect(Collectors.toList());
             System.out.println("Parsed employment records: " + employmentRecords.toString());
 
-            //JAXB Parser - transcript
+            //endregion
+
+            //region JAXB Parser - transcript
+
             JAXBContext jaxbTranscriptContext = JAXBContext.newInstance(Transcript.class);
             Unmarshaller jaxbTranscriptUnmarshaller = jaxbTranscriptContext.createUnmarshaller();
             Transcript transcript = (Transcript) jaxbTranscriptUnmarshaller.unmarshal(transcriptFile);
             System.out.println("Parsed transcript: " + transcript.toString());
 
-            //JAXB Parser - resume
+            //endregion
+
+            //region JAXB Parser - resume
+
             JAXBContext jaxbResumeContext = JAXBContext.newInstance(Resume.class);
             Unmarshaller jaxbResumeUnmarshaller = jaxbResumeContext.createUnmarshaller();
             Resume resume = (Resume) jaxbResumeUnmarshaller.unmarshal(resumeFile);
             System.out.println("Parsed resume: " + resume.toString());
 
-            // XSLT
+            //endregion
+
+            //region XSLT
+
             // http://stackoverflow.com/questions/4604497/xslt-processing-with-java
             // http://stackoverflow.com/questions/1384802/java-how-to-indent-xml-generated-by-transformer
+
+            // Generate applicant profile from original XML documents
 
             File applicantProfileFromOriginalXmlXslFile = new File(
                     getClass().getClassLoader().getResource("xml/ApplicantProfileFromOriginalXml.xsl").getFile()
@@ -114,6 +129,8 @@ public class Program
                             new File("output_xml/ApplicantProfileFromOriginalXml.xml")
                     )
             );
+
+            //endregion
         }
         catch (IOException|SAXException|ParserConfigurationException|JAXBException|TransformerException ex)
         {
