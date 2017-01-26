a159e28b30e9b72d9fd08bf15f652b58c8102249
Fannar Magnússon <fannar@kth.se>
2017-01-26 11:51:45 +0100

    get ssn from employment records

--- hw1/src/main/java/SAXHandler.java
+++ hw1/src/main/java/SAXHandler.java
@@ -4,29 +4,32 @@ import org.xml.sax.helpers.DefaultHandler;
 
 public class SAXHandler extends DefaultHandler {
 
+    private String currentElement;
     public SAXHandler() {
+        this.currentElement = "";
     }
 
     public void startDocument() {
-        System.out.println("***Start of Document***");
+        System.out.println("SAXHandler starting parsing");
     }
 
     public void endDocument() {
-        System.out.println("***End of Document***");
+        System.out.println("SAXHandler ending parsing");
     }
 
     public void startElement(String uri, String localName, String qName, Attributes attributes) {
-        System.out.print("<" + qName);
-        int n = attributes.getLength();
-        for (int i = 0; i < n; i += 1) {
-            System.out.print(" " + attributes.getQName(i) + "='" + attributes.getValue(i) + "'");
-        }
-        System.out.println(">");
+
+        this.currentElement = qName;
     }
     public void characters(char[] ch, int start, int length) {
-        System.out.println(new String(ch, start, length).trim());
+        //System.out.println(new String(ch, start, length).trim());
+
+        if(this.currentElement.equals("ssn")) {
+            System.out.println("We got the ssn from employment records, it is: " + new String(ch, start, length).trim());
+        }
     }
     public void endElement(String namespaceURI, String localName, String qName) throws SAXException {
-        System.out.println("</" + qName + ">");
+        //System.out.println("</" + qName + ">");
+        this.currentElement = "";
     }
 }
