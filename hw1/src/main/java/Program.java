dc6172dca21a44fea3c4eacd58d5cc72f472f02e
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2017-01-26 16:02:38 +0100

    adding ApplicantProfileFromAll xsl

--- hw1/src/main/java/Program.java
+++ hw1/src/main/java/Program.java
@@ -6,6 +6,9 @@ import java.util.ArrayList;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.*;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 import java.io.*;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -61,11 +64,35 @@ public class Program
             List<EmploymentRecord> employmentRecords = saxHandlerObjects.stream()
                     .map(object -> (EmploymentRecord)object)
                     .collect(Collectors.toList());
+
+            // XSLT
+            // http://stackoverflow.com/questions/4604497/xslt-processing-with-java
+            // http://stackoverflow.com/questions/1384802/java-how-to-indent-xml-generated-by-transformer
+
+            File applicantProfileFromAllXslFile = new File(
+                    getClass().getClassLoader().getResource("xml/ApplicantProfileFromAll.xsl").getFile()
+            );
+            StreamSource xsl = new StreamSource(applicantProfileFromAllXslFile);
+
+            TransformerFactory factory = TransformerFactory.newInstance();
+            Transformer transformer = factory.newTransformer(xsl);
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+
+            // Set indentation of output document
+            // TODO Indentation not working
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+
+            transformer.transform(
+                    new StreamSource(),
+                    new StreamResult(
+                            new File("output_xml/ApplicantProfileFromAll.xml")
+                    )
+            );
         }
-        catch (IOException|SAXException|ParserConfigurationException ex)
+        catch (IOException|SAXException|ParserConfigurationException|TransformerException ex)
         {
             System.err.println(ex);
-            return;
         }
     }
 }
