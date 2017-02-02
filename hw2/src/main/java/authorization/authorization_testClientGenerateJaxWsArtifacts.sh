9d9ea7aee12aaf870de9afe43310936f3af2b4cf
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2017-02-02 17:10:21 +0100

    echo

--- hw2/src/main/java/authorization/authorization_testClientGenerateJaxWsArtifacts.sh
+++ hw2/src/main/java/authorization/authorization_testClientGenerateJaxWsArtifacts.sh
@@ -1,3 +1,5 @@
  #!/bin/bash
 
+echo "Generating authorization test client JAX-WS artifacts"
+
  wsimport -keep -d "../" -cp "../" -p authorization.testclient http://localhost:8080/AuthorizationService/authorization?wsdl
