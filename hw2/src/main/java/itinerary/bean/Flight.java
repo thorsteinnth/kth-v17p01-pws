13f8ac4cc9988428c1bda36b6551f67354c5c50d
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2017-02-02 18:22:29 +0100

    rename ItineraryRequest to Flight

---
+++ hw2/src/main/java/itinerary/bean/Flight.java
@@ -0,0 +1,27 @@
+package itinerary.bean;
+
+public class Flight
+{
+    private String departure;
+    private String destination;
+
+    public String getDeparture()
+    {
+        return departure;
+    }
+
+    public void setDeparture(String departure)
+    {
+        this.departure = departure;
+    }
+
+    public String getDestination()
+    {
+        return destination;
+    }
+
+    public void setDestination(String destination)
+    {
+        this.destination = destination;
+    }
+}
