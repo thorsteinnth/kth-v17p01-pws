55946ac0eaa7ba18c1d98949c238be2ea44b06cf
Fannar Magnússon <fannar@kth.se>
2017-02-02 15:28:28 +0100

    authorize user

--- hw2/src/main/java/authorization/service/AuthorizationService.java
+++ hw2/src/main/java/authorization/service/AuthorizationService.java
@@ -5,6 +5,7 @@ import authorization.bean.User;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
+import java.util.ArrayList;
 
 @WebService(serviceName = "Authorization",
         portName = "AuthorizationPort",
@@ -14,13 +15,39 @@ import javax.jws.soap.SOAPBinding;
 
 public class AuthorizationService {
 
+    private ArrayList<User> authorizedUsers;
+
+    public AuthorizationService() {
+        generateAuthorizedUsers();
+    }
+
     @WebMethod
     public User authorizeUser(User user) {
 
-        // TODO : check if is valid user
+        String securityToken = "";
 
-        user.setToken("secure token");
+        for (User authorizedUser : this.authorizedUsers) {
+            if (authorizedUser.getUsername().equals(user.getUsername())) {
+                if (authorizedUser.getPassword().equals(user.getPassword())) {
+                    securityToken = "secure token";
+                    break;
+                }
+            }
+        }
 
+        user.setToken(securityToken);
         return user;
+    }
+
+    private void generateAuthorizedUsers() {
+
+        this.authorizedUsers = new ArrayList<User>();
+
+        for (int counter = 1; counter < 11; counter++) {
+            User user = new User();
+            user.setUsername("user" + Integer.toString(counter));
+            user.setPassword("pass" + Integer.toString(counter));
+            this.authorizedUsers.add(user);
+        }
     }
 }
