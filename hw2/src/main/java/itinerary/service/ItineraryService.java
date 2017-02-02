9307afe3539d51a3289c3da8fa4f5f078898cadd
Fannar Magnússon <fannar@kth.se>
2017-02-02 18:34:30 +0100

    find itinerary web method added

--- hw2/src/main/java/itinerary/service/ItineraryService.java
+++ hw2/src/main/java/itinerary/service/ItineraryService.java
@@ -1,5 +1,24 @@
 package itinerary.service;
 
+import itinerary.bean.Flight;
+import itinerary.bean.Itinerary;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+@WebService(serviceName = "Itinerary",
+        portName = "AuthorizationPort",
+        targetNamespace = "http://hw2.flightticketreservation/authorization.service/authorization")
+
+@SOAPBinding(style=SOAPBinding.Style.DOCUMENT,use=SOAPBinding.Use.LITERAL,parameterStyle=SOAPBinding.ParameterStyle.WRAPPED)
+
 public class ItineraryService
 {
+    @WebMethod
+    public Itinerary findItinerary(Flight flight) {
+
+        Itinerary it = new Itinerary();
+        return it;
+    }
 }
