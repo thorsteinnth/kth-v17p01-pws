5b901ec6217a2b88d695efd3d80e368c01eabfa1
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2017-02-02 15:02:42 +0100

    authorization service publisher

--- hw2/src/main/java/authorization/publish/AuthorizationServicePublisher.java
+++ hw2/src/main/java/authorization/publish/AuthorizationServicePublisher.java
@@ -1,5 +1,15 @@
 package authorization.publish;
 
+import javax.xml.ws.Endpoint;
+
+import authorization.service.AuthorizationService;
+
 public class AuthorizationServicePublisher
 {
+    public static void main(String[] args)
+    {
+        final String WSUrl = "http://localhost:8080/AuthorizationService/authorization";
+        Endpoint.publish(WSUrl, new AuthorizationService());
+        System.out.println("The authorization service is published at: " + WSUrl);
+    }
 }
