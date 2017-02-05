a695bb016ce7d2a301277c3e5082cda420935497
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2017-02-05 22:05:31 +0100

    rename

--- hw2/src/main/java/itinerary/itinerary_testClientGenerateJaxWsArtifacts.sh
+++ hw2/src/main/java/itinerary/itinerary_testClientGenerateJaxWsArtifacts.sh
@@ -2,4 +2,4 @@
 
 echo "Generating itinerary test client JAX-WS artifacts"
 
-wsimport -keep -b "testclient/jaxws_bindings.xml" -d "../" -cp "../" -p itinerary.testclient http://localhost:8080/ItineraryService/itinerary?wsdl
+wsimport -keep -b "testclient/itinerary_jaxws_bindings.xml" -d "../" -cp "../" -p itinerary.testclient http://localhost:8080/ItineraryService/itinerary?wsdl
