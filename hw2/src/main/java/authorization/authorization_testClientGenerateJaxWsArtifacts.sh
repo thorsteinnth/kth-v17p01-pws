ee9d543b257d3c6c6d197f7a6471252fddb97d11
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2017-02-06 21:17:20 +0100

    fix formatting

--- hw2/src/main/java/authorization/authorization_testClientGenerateJaxWsArtifacts.sh
+++ hw2/src/main/java/authorization/authorization_testClientGenerateJaxWsArtifacts.sh
@@ -1,5 +1,5 @@
- #!/bin/bash
+#!/bin/bash
 
 echo "Generating authorization test client JAX-WS artifacts"
 
- wsimport -keep -d "../" -cp "../" -p authorization.testclient http://localhost:8080/AuthorizationService/authorization?wsdl
+wsimport -keep -d "../" -cp "../" -p authorization.testclient http://localhost:8080/AuthorizationService/authorization?wsdl
