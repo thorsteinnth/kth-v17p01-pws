287f83fa02f8961d8f96e977ec8db9616c20c435
Fannar Magnússon <fannar@kth.se>
2017-02-10 17:01:36 +0100

    MainClient created - test hello service and more

--- hw3/src/main/java/client/MainClient.java
+++ hw3/src/main/java/client/MainClient.java
@@ -6,11 +6,26 @@ import javax.ws.rs.client.WebTarget;
 
 public class MainClient {
 
+    private static String baseURL = "http://localhost:8080/hello";
+
+    private Client webClient;
+    private WebTarget webTarget;
+
     public static void main(String[] args) {
 
-        Client client = ClientBuilder.newClient();
-        WebTarget webTarget = client.target("http://localhost:8080/hello");
+        MainClient mainClient = new MainClient();
+        mainClient.testHelloResource();
+    }
+
+    public MainClient() {
+        this.webClient = ClientBuilder.newClient();
+        this.webTarget = webClient.target(baseURL);
+    }
+
+    private void testHelloResource() {
         String response = webTarget.request().get(String.class);
+        System.out.println("Testing hello resource..");
         System.out.println(response);
     }
+
 }
