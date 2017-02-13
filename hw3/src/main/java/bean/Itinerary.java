bc241d3b887a4e8397a6a19f50162c455370d283
Fannar Magnússon <fannar@kth.se>
2017-02-13 15:27:39 +0100

    new extended itinerary class instead of bookable itinerary

--- hw3/src/main/java/bean/Itinerary.java
+++ hw3/src/main/java/bean/Itinerary.java
@@ -8,6 +8,9 @@ import java.util.List;
 public class Itinerary {
 
     private List<Flight> flights;
+    private String date;  // date needs to be on the format yyyy-MM-dd
+    private int totalPrice;
+    private int numberOfAvailableTickets;
 
     @XmlElement
     public List<Flight> getFlights() {
@@ -18,10 +21,40 @@ public class Itinerary {
         this.flights = flights;
     }
 
+    @XmlElement
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @XmlElement
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    @XmlElement
+    public int getNumberOfAvailableTickets() {
+        return numberOfAvailableTickets;
+    }
+
+    public void setNumberOfAvailableTickets(int numberOfAvailableTickets) {
+        this.numberOfAvailableTickets = numberOfAvailableTickets;
+    }
+
     @Override
     public String toString() {
         return "Itinerary{" +
                 "flights=" + flights +
+                ", date='" + date + '\'' +
+                ", totalPrice=" + totalPrice +
+                ", numberOfAvailableTickets=" + numberOfAvailableTickets +
                 '}';
     }
 }
