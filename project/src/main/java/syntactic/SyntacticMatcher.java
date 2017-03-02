7c1d0e76f7332801d4883f6a16481c2b526ec0ae
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2017-03-02 16:50:56 +0100

    start parsing wsdls

--- project/src/main/java/syntactic/SyntacticMatcher.java
+++ project/src/main/java/syntactic/SyntacticMatcher.java
@@ -2,13 +2,26 @@ package syntactic;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.namespace.NamespaceContext;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.*;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
+import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Iterator;
 
 public class SyntacticMatcher
 {
     private final static Logger LOG = LoggerFactory.getLogger(SyntacticMatcher.class);
+    private DocumentBuilderFactory documentBuilderFactory;
+    private XPathFactory xPathfactory;
 
     public static void main(String[] args)
     {
@@ -18,6 +31,10 @@ public class SyntacticMatcher
 
     public SyntacticMatcher()
     {
+        this.documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        this.documentBuilderFactory.setNamespaceAware(true);
+        this.xPathfactory = XPathFactory.newInstance();
+
         File[] WSDLs = getWSDLs();
 
         for (int i=0; i<WSDLs.length; i++)
@@ -30,8 +47,44 @@ public class SyntacticMatcher
                     continue;
                 }
 
-                LOG.debug("Should compare WSDLs: " + WSDLs[i].toString() + " - " + WSDLs[y].toString());
+                compare(WSDLs[i], WSDLs[y]);
             }
+        }
+    }
+
+    /**
+     * Compare outputs of operations of wsdl1 with inputs of operations of wsdl2
+     * @param wsdl1
+     * @param wsdl2
+     */
+    private void compare(File wsdl1, File wsdl2)
+    {
+        //LOG.debug("Should compare WSDLs: " + wsdl1.toString() + " - " + wsdl2.toString());
+        parseOutputOfOperations(wsdl1);
+    }
+
+    private void parseOutputOfOperations(File wsdl)
+    {
+        try
+        {
+            DocumentBuilder builder = this.documentBuilderFactory.newDocumentBuilder();
+            Document doc = builder.parse(wsdl);
+
+            XPath xpath = xPathfactory.newXPath();
+            xpath.setNamespaceContext(getWsdlNamespaceContext());
+
+            XPathExpression expr = xpath.compile("//wsdl:portType");
+            NodeList nodeList = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
+
+            for (int i = 0; i<nodeList.getLength(); i++)
+            {
+                Node node = nodeList.item(i);
+                System.out.println("Found node: " + node.getLocalName());
+            }
+        }
+        catch (ParserConfigurationException|SAXException|XPathExpressionException|IOException ex)
+        {
+            LOG.error(ex.toString());
         }
     }
 
@@ -39,13 +92,36 @@ public class SyntacticMatcher
     {
         try
         {
-            File[] WSDLs = (new File(getClass().getResource("/WSDLs").toURI())).listFiles();
-            return WSDLs;
+            return (new File(getClass().getResource("/WSDLs").toURI())).listFiles();
         }
         catch (URISyntaxException ex)
         {
             LOG.error(ex.toString());
             return new File[0];
         }
+    }
+
+    private NamespaceContext getWsdlNamespaceContext()
+    {
+        return new NamespaceContext()
+        {
+            @Override
+            public String getNamespaceURI(String prefix)
+            {
+                return prefix.equals("wsdl") ? "http://schemas.xmlsoap.org/wsdl/" : null;
+            }
+
+            @Override
+            public String getPrefix(String namespaceURI)
+            {
+                return null;
+            }
+
+            @Override
+            public Iterator getPrefixes(String namespaceURI)
+            {
+                return null;
+            }
+        };
     }
 }
