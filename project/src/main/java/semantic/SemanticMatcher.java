a8e0092c62ca18c0abe935940671130fbde48dbb
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2017-03-05 17:35:35 +0100

    semanticmatcher - parsing services

--- project/src/main/java/semantic/SemanticMatcher.java
+++ project/src/main/java/semantic/SemanticMatcher.java
@@ -1,5 +1,6 @@
 package semantic;
 
+import common.Parser;
 import common.ServiceContainer;
 import ontology.MyOntManager;
 import org.mindswap.pellet.owlapi.Reasoner;
@@ -8,11 +9,12 @@ import org.semanticweb.owl.model.OWLOntology;
 import org.semanticweb.owl.model.OWLOntologyManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.xml.sax.SAXException;
 
 import java.io.File;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class SemanticMatcher {
 
@@ -24,81 +26,50 @@ public class SemanticMatcher {
     private String ontLocation = "file:data/SUMO.owl";
     private MyOntManager ontsum = null;
     private HashMap<String, OWLClass> mapName_OWLClass;
+    private List<List<ServiceContainer>> parsedServiceContainers;
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         LOG.debug("Starting Semantic Matcher...");
         new SemanticMatcher();
     }
 
-    public SemanticMatcher() {
+    public SemanticMatcher()
+    {
+        parseSAWSDLs();
 
         this.ontsum = new  MyOntManager();
         this.manager = ontsum.initializeOntologyManager();
         this.ontology = ontsum.initializeOntology(manager, ontLocation);
         this.reasoner = ontsum.initializeReasoner(ontology, manager);
         this.mapName_OWLClass = ontsum.loadClasses(reasoner);
-
-        File[] SAWSDLs = getSAWSDLs();
-
-        for (int i=0; i < SAWSDLs.length; i++)
-        {
-            ServiceContainer outputService;
-            ServiceContainer inputService;
-
-            try
-            {
-                outputService = parseService(SAWSDLs[i]);
-                LOG.debug("OutputService - parsed service for: " + SAWSDLs[i] + " - " + outputService);
-            }
-            catch (SAXException ex)
-            {
-                LOG.error("Unable to parse output service " + SAWSDLs[i] + " - " + ex.toString());
-                continue;
-            }
-
-            for (int y=0; y < SAWSDLs.length; y++)
-            {
-                if (i == y)
-                {
-                    // Don't want to compare a file to itself
-                    continue;
-                }
-
-                try
-                {
-                    inputService = parseService(SAWSDLs[y]);
-                    LOG.debug("InputService - parsed service for: " + SAWSDLs[y] + " - " + inputService);
-                }
-                catch (SAXException ex)
-                {
-                    LOG.error("Unable to parse input service " + SAWSDLs[y] + " - " + ex.toString());
-                    continue;
-                }
-
-                /*
-                if (outputService != null && outputService.portTypeContainers != null
-                        && inputService != null && inputService.portTypeContainers != null)
-                    compare(outputService, inputService);
-                    */
-            }
-
-            break;
-        }
     }
 
-    // TODO : We need to parse the SAWSDL files
     // TODO : Find Service -> Port -> PortType -> Operations (input / output) -> MessageContainer (has multiple parts/elements)
     // TODO : -> for each part/element find the type, then lookup the the element name in the xsd:schema (by element type)
     // TODO : - the element name can be used as a parameter into the getMatchingDegreeFunction
 
-    private void compare(ServiceContainer outputService, ServiceContainer inputService) {
-
+    private void parseSAWSDLs()
+    {
+        File[] SAWSDLs = getSAWSDLs();
+        this.parsedServiceContainers = new ArrayList<>();
+        for (int i = 0; i < SAWSDLs.length; i++)
+        {
+            try
+            {
+                List<ServiceContainer> serviceContainers = Parser.parseServices(SAWSDLs[i]);
+                this.parsedServiceContainers.add(serviceContainers);
+                LOG.info("Parsed services for SAWSDL " + i + ": " + SAWSDLs[i] + " - " + serviceContainers);
+            }
+            catch (Exception ex)
+            {
+                LOG.error("Could not parse SAWSDL: " + SAWSDLs[i] + " - " + ex.toString());
+            }
+        }
     }
 
-    private ServiceContainer parseService(File sawsdl) throws SAXException{
-
-        return null;
-    }
+    private void compare(ServiceContainer outputService, ServiceContainer inputService)
+    {}
 
     /**
      * Calculates the matching degree for higher level types that can all be found in
